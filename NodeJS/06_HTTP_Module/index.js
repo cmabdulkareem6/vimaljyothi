396729fc809fe9fbcc2a41cea252373b9c477ccf
“[abdul <[www.cmakareem@gmail.com]>
2025-08-21 14:38:34 +0530

    updated

--- NodeJS/06_HTTP_Module/index.js
+++ NodeJS/06_HTTP_Module/index.js
@@ -1,35 +1,18 @@
-// index.js
-
 import http from 'http';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
+import path, {dirname} from 'path';
 
-// Recreate __dirname (since we are in ES modules)
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
-// Create HTTP server
-const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        // Serve index.html
-        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    } else {
-        // Handle other routes
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('404 Not Found');
+const server = http.createServer((req, res)=>{
+    if(req.url === "/"){
+        console.log("you are on home page")
+    }else{
+        console.log("Error 404")
     }
-});
+})
 
-// Start server
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
-});
+server.listen(3000, ()=>{
+    console.log("server is listening on port 3000")
+})

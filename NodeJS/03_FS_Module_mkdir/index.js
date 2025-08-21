3fbe98355d11e58af152832e221bcbe2c1e94158
“[abdul <[www.cmakareem@gmail.com]>
2025-08-21 14:47:16 +0530

    updated

--- NodeJS/03_FS_Module_mkdir/index.js
+++ NodeJS/03_FS_Module_mkdir/index.js
@@ -1,20 +1,9 @@
-import fs from 'fs';
+import fs   from 'fs'
 
-fs.mkdir('folderName', { recursive: true }, (err) => {
+fs.mkdir('new_folder', { recursive: true }, (err) => {
     if (err) {
         console.error(err);
     } else {
         console.log('Folder created successfully');
     }
 });
-
-/*
-fs.mkdir() is used to create a new directory
-
-fs.mkdir() accepts 3 parameters
-1. directory (folder) name
-2. {recursive: true}
-we can use recursive option to create a directory inside a directory
-3. callback function (a function that will be executed after the directory is created)
-callback function can contain error object as a parameter, so that we can handle errors
-*/
